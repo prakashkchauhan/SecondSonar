2c8a583108bd4bb497adb1c8fd8244701023abaf
prakashkchauhan <prakashkchauhan@gmail.com>
2023-06-19 18:16:25 +0530

    Add files via upload

---
+++ src/main/java/com/sonar/SecondSonarApplication.java
@@ -0,0 +1,13 @@
+package com.sonar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecondSonarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecondSonarApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sonar/controller/SonarController.java
@@ -0,0 +1,13 @@
+package com.sonar.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SonarController {
+
+	@GetMapping("/sonarTest")
+	public String test() {
+		return "Hello World";
+	}
+}
