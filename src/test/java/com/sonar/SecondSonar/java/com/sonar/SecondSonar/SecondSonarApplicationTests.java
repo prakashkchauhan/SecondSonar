59a5801700f9ac70f60f46569b5d08efdb60b3c4
prakashkchauhan <prakashkchauhan@gmail.com>
2023-06-20 15:17:06 +0530

    Add files via upload

---
+++ src/test/java/com/sonar/SecondSonar/java/com/sonar/SecondSonar/SecondSonarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonar.SecondSonar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecondSonarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ src/test/java/com/sonar/SecondSonar/java/com/sonar/controller/SonarControllerTest.java
@@ -0,0 +1,28 @@
+package com.sonar.controller;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class SonarControllerTest {
+
+	private SonarController sonarController;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		sonarController = new SonarController();
+	}
+
+	@AfterEach
+	void tearDown() throws Exception {
+	}
+
+	
+	@Test
+	void testTest() {
+		String name = sonarController.test();
+		Assert.assertNotNull(name);
+	}
+
+}
