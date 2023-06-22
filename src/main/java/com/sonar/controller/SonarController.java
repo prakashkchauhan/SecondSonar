dd1fe5968bdc30ecb3b1f372e4cf9c93cee8ffca
Prakash <prakashkchauhan@gmail.com>
2023-06-22 16:12:15 +0530

    Updated Junit test case.

--- src/main/java/com/sonar/controller/SonarController.java
+++ src/main/java/com/sonar/controller/SonarController.java
@@ -8,14 +8,7 @@ public class SonarController {
 
 	@GetMapping("/sonarTest")
 	public String test() {
-		if(false) {
-			System.out.println("test");
-			m1();
-		}
 		return "Hello World";
 	}
 
-	private void m1() {
-		System.out.print("method m1");
-	}
 }
--- src/test/java/com/sonar/SecondSonar/SecondSonarApplicationTests.java
+++ src/test/java/com/sonar/SecondSonar/SecondSonarApplicationTests.java
@@ -3,11 +3,14 @@ package com.sonar.SecondSonar;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.sonar.SecondSonarApplication;
+
 @SpringBootTest
 class SecondSonar2ApplicationTests {
 
 	@Test
 	void contextLoads() {
+		SecondSonarApplication.main(new String[] {});
 	}
 
 }
